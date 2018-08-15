d64d276f92b23263e3d9df84534cd9cb99906d0d
Tobias Wojtylak <tw@solutiondrive.de>
2018-08-15 16:29:22 +0200

    do not pass first argument (PHP Version) to docker exec

--- bin/runInSdTest.sh
+++ bin/runInSdTest.sh
@@ -29,5 +29,4 @@ PROJECT_NAME="$( basename ${PROJECT_DIR} | tr '[:upper:]' '[:lower:]' )"
 PHP_CONTAINER_NAME="${PROJECT_NAME}_shopware546_php${VERSION}_1"
 WORK_DIR="/var/www/shopware54_php${VERSION}"
 
-docker exec --workdir ${WORK_DIR} -it ${PHP_CONTAINER_NAME} $@
-
+docker exec --workdir ${WORK_DIR} -it ${PHP_CONTAINER_NAME} ${@:2}
