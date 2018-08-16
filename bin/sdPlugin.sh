337057a4b4bccfeac95af01b4d3fa99a2c782371
Tobias Wojtylak <tw@solutiondrive.de>
2018-08-16 16:32:50 +0200

    fixed indentation for execute_in_docker

--- bin/sdPlugin.sh
+++ bin/sdPlugin.sh
@@ -66,7 +66,7 @@ function remove_plugin {
 }
 
 function execute_in_docker {
- docker exec --workdir ${WORK_DIR} -it ${PHP_CONTAINER_NAME} $1
+    docker exec --workdir ${WORK_DIR} -it ${PHP_CONTAINER_NAME} $1
 }
 
 ## start of the real program
